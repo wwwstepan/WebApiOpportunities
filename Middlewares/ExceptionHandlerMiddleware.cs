ae72f36f4fde4aa30913f23ccd1e334e4203f510
Stepan Purk <63333004+wwwstepan@users.noreply.github.com>
2024-02-14 02:04:57 +0300

    Add files via upload

---
+++ Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System.Text.Json;
+using WebApiOpportunities.Contracts;
+
+namespace WebApiOpportunities.Middlewares;
+
+public class ExceptionHandlerMiddleware
+{
+    private readonly RequestDelegate? _next;
+    private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+
+    public ExceptionHandlerMiddleware(RequestDelegate? next, ILogger<ExceptionHandlerMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (KeyNotFoundException ex)
+        {
+            await HandleExceptionAsync(httpContext, ex.Message, HttpStatusCode.NotFound, "Key not found");
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(httpContext, ex.Message, HttpStatusCode.InternalServerError, "Internal server error");
+        }
+    }
+
+    public async Task HandleExceptionAsync(
+        HttpContext httpContext,
+        string exceptionMessage,
+        HttpStatusCode httpStatusCode,
+        string message
+        )
+    {
+        _logger.LogError(exceptionMessage);
+
+        var response = httpContext.Response;
+        response.ContentType = "application/json";
+        response.StatusCode = (int)httpStatusCode;
+
+        var errorDto = new ErrorDto
+        {
+            Code = response.StatusCode,
+            Message = message
+        };
+
+        var result = JsonSerializer.Serialize(errorDto);
+
+        await response.WriteAsync(result);
+    }
+}
